fab3acbd8fed42cf589a712bc2d3530fef4b7054
moAllElite <mouhamedn347@gmail.com>
2023-02-26 17:29:38 +0000

    first angular project with OC reload !!!

--- src/app/snap/snap.component.ts
+++ src/app/snap/snap.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FaceSnapModule} from "../models/face-snap/face-snap.module";
 
 @Component({
   selector: 'app-snap',
   templateUrl: './snap.component.html',
   styleUrls: ['./snap.component.css']
 })
-export class SnapComponent {
-
+export class SnapComponent implements OnInit{
+  @Input() mysnap!:FaceSnapModule;
+  title?:string;
+  description?:string;
+  imageUrl?:string;
+  snap!:number;
+  buttonText?:string
+  ngOnInit() {/*
+    this.title='mo';
+    this.description='fu pottere';
+    this.imageUrl='./assets/images/img1.jpg';
+    this.snap=0;*/
+  }
+  onSnap(){
+    if (this.buttonText==="snap") {
+      this.buttonText = "oops snap";
+      this.snap++;
+    }else {
+      this.buttonText=" snap";
+      this.snap--;
+    }
+  }
 }
