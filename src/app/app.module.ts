ac8cf8ce59e01509e67fa0b3bf876bf067468a0d
moAllElite <mouhamedn347@gmail.com>
2023-02-26 14:58:58 +0000

    first angular project with OC reload !!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { SnapComponent } from './snap/snap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SnapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, far,fab);
+  }
+}
---
+++ src/app/snap/snap.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-snap',
+  templateUrl: './snap.component.html',
+  styleUrls: ['./snap.component.css']
+})
+export class SnapComponent {
+
+}
