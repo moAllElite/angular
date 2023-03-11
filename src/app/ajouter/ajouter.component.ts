2032d9519679c0775b8eb7160d1180e3f0bfcd77
moAllElite <mouhamedn347@gmail.com>
2023-03-11 23:37:17 +0000

    first angular project with OC reload !!!

--- src/app/ajouter/ajouter.component.ts
+++ src/app/ajouter/ajouter.component.ts
@@ -1,4 +1,5 @@
 import { Component ,NgModule,OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 
 @Component({
@@ -7,7 +8,18 @@ import { Component ,NgModule,OnInit} from '@angular/core';
   styleUrls: ['./ajouter.component.css']
 })
 export class AjouterComponent implements OnInit{
+  titleToAdd='';
+  priceToAdd='';
+  commentToAdd='';
   ngOnInit(): void {
   }
 
+  onSubmit(form:NgForm) {
+    const newTitle=this.titleToAdd;
+    const newPrice=this.priceToAdd;
+    const newComment=this.commentToAdd;
+    console.log("new title:"+newTitle);
+    console.log("new price :" +newPrice);
+    console.log("new comment :"+newComment);
+  }
 }
